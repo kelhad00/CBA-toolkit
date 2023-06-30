63c89903422aaa9426d3f3b6034fe8d15108dd04
Antoine Bonnafous <antoine.bonnafous@student.junia.com>
2023-06-30 11:07:42 +0200

    Update icones + Plus de Pages

--- Streamlit/Affichage_pattern.py
+++ Streamlit/Affichage_pattern.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from PIL import Image
-
+import os
 
 def affichage():
-    img=Image.open('images\ISIA_Lab.jpg')
+    image_path = os.path.join('images', 'ISIA_Lab.jpg')
+    img = Image.open(image_path)
     st.set_page_config(page_title="SNL Analysis", page_icon=img, layout='centered')
     st.markdown("""
     <style>
