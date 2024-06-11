545ae77b37fb2f40c871e44f25cb7476631549f2
Antoine Maes <92756820+AntoLeRigoloo@users.noreply.github.com>
2024-06-11 12:34:41 +0200

    fix launch app from terminal

--- Dash/app.py
+++ Dash/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import dash
 from dash import Dash, html, dcc, Output, Input
 import dash_mantine_components as dmc
